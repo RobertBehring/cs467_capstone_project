100aa7df083b3d0583be00392c45f514c4966a57
jry2112 <jry2112@columbia.edu>
2023-02-01 00:58:34 -0500

    updated descriptions and functions

--- tools/gcp_storage.py
+++ tools/gcp_storage.py
@@ -1,11 +1,15 @@
-from dotenv import dotenv_values
-
+import os
+from dotenv import load_dotenv
 from google.cloud import storage
 
-config = dotenv_values(".env")
+load_dotenv()
+STORAGE_BUCKET_NAME = os.getenv("STORAGE_BUCKET_NAME")
 
 # User must first configure Google Cloud Credentials. See README for more details.
-# Needs to be tested in current environment so will not work yet
+"""
+Functions for interacting with GCP Cloud Storage. Creates a Storage Client and populates a list containing the names of available buckets.
+Allows user to: 1) view buckets and their contents, 2) upload data to buckets 
+"""
 class GCPStorage:
     def __init__(self) -> None:
         self.storage_client = storage.Client()
@@ -14,22 +18,27 @@ class GCPStorage:
 
 
     def populate_buckets(self):
+        """
+        Retrives the names of all buckets available in the storage cloud and stores them
+        in a list. https://cloud.google.com/storage/docs/listing-buckets#storage-list-buckets-python
+        """
         buckets = self.storage_client.list_buckets()
         for bucket in buckets:
             self.buckets.append(bucket.name)
 
-    def list_buckets(self):
-        """Lists all buckets: https://cloud.google.com/storage/docs/listing-buckets#storage-list-buckets-python"""
+    def print_buckets(self):
+        """Prints all bucket names. """
         buckets = self.storage_client.list_buckets()
-        for bucket in buckets:
+        for bucket in self.buckets:
             print(bucket.name)
 
     def make_bucket(self, bucket_name:str):
-        """Makes a bucket"""
+        """Makes a bucket."""
         bucket = self.storage_client.create_bucket(bucket_name)
         print(f"Bucket {bucket.name} created.")
 
-    def view_all_buckets(self):
+    def view_contents_all_buckets(self):
+        """Prints contents of all buckets"""
         for bucket in self.buckets:
             print(bucket)
             blobs = self.storage_client.list_blobs(bucket)
@@ -42,3 +51,12 @@ class GCPStorage:
         blobs = self.storage_client.list_blobs(bucket_name)
         for blob in blobs:
             print(blob.name)
+
+    # TODO: function for uploading json file to bucket 
+    def upload_to_bucket(self, bucket_name, file_path):
+        pass
+
+if __name__ == "__main__":
+    storage = GCPStorage()
+    storage.list_buckets()
+    storage.view_a_bucket(STORAGE_BUCKET_NAME)
